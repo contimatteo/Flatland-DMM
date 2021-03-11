afa445b7794297caee15fbd241ba35eebcd8ebf9
Manuel <pq.manuel.pq@gmail.com>
2021-03-11 17:36:13 +0100

    simple example using random agent

---
+++ src/random_agent.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+
+class RandomAgent:
+
+    def __init__(self, state_size, action_size):
+        self.state_size = state_size
+        self.action_size = action_size
+        np.random.seed(1)
+
+    def act(self, state):
+        """
+        :param state: input is the observation of the agent
+        :return: returns an action
+        """
+        return np.random.choice(np.arange(self.action_size))
+
+    def step(self, memories):
+        """
+        Step function to improve agent by adjusting policy given the observations
+
+        :param memories: SARS Tuple to be
+        :return:
+        """
+        return
+
+    def save(self, filename):
+        # Store the current policy
+        return
+
+    def load(self, filename):
+        # Load a policy
+        return
