66f861f502bc54f918a6a7c131197e433798c9ba
Conti Matteo <contimatteo1505@gmail.com>
2021-03-11 11:15:14 +0100

    simple example of unittesting

--- tests/setup_test.py
+++ tests/setup_test.py
@@ -1,16 +1,18 @@
 import unittest
+import numpy as np
 
 
 class TestStringMethods(unittest.TestCase):
     def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+        self.assertEqual('hello'.upper(), 'HELLO')
 
     def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
+        self.assertTrue('HELLO'.isupper())
+        self.assertFalse('Hello'.isupper())
 
     def test_split(self):
-        msg = 'hello world'
+        chars = ['h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd']
+        msg = ''.join(np.array(chars, dtype=str).tolist())
         self.assertEqual(msg.split(), ['hello', 'world'])
         # check that s.split fails when the separator is not a string
         with self.assertRaises(TypeError):
