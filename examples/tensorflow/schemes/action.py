b4a09566f456644b85fbf224f1823f54328d4fb8
Conti Matteo <contimatteo1505@gmail.com>
2021-08-18 17:03:34 +0200

    high level actions

--- examples/tensorflow/schemes/action.py
+++ examples/tensorflow/schemes/action.py
@@ -1,5 +1,15 @@
+from enum import IntEnum
+
 from flatland.envs.rail_env import RailEnvActions
 
 ###
 
-Action = RailEnvActions
+LowLevelActions = RailEnvActions
+
+###
+
+
+class HighLevelActions(IntEnum):
+    STOP = 0
+    LEFT_ORIENTED_ACTION = 1
+    RIGHT_ORIENTED_ACTION = 1
