ab5f25c690dc78dc9033e008077da8395ae625fa
Conti Matteo <contimatteo1505@gmail.com>
2021-09-06 12:59:38 +0200

    start creating new `src` folder.

--- src/configs.py
+++ src/configs.py
@@ -1,18 +1,20 @@
-#
+#########
+## APP ##
+#########
 
 APP_DEBUG: bool = True
 APP_SEED: int = 100
 
-#######################
-## Flatland Rail Env ##
-#######################
+##############
+## RAIL ENV ##
+##############
 
 RAIL_ENV_WIDTH: int = 15
 RAIL_ENV_HEIGHT: int = 15
 RAIL_ENV_N_CELLS: int = RAIL_ENV_WIDTH * RAIL_ENV_HEIGHT
 
 ##############
-## Emulator ##
+## EMULATOR ##
 ##############
 
 EMULATOR_ACTIVE: bool = False
@@ -20,24 +22,23 @@ EMULATOR_WINDOW_WIDTH: int = 1200
 EMULATOR_WINDOW_HEIGHT: int = 1200
 EMULATOR_STEP_TIMEBREAK_SECONDS: int = 0.3
 
-##############
-## Training ##
-##############
+###########
+## TRAIN ##
+###########
 
-N_OF_AGENTS: int = 1
-
+TRAIN_N_AGENTS: int = 1
 TRAIN_N_ATTEMPTS = 5
 TRAIN_N_EPISODES = 100
 
-####################
-## Neural Network ##
-####################
+########
+## NN ##
+########
 
 NN_VERBOSE = False
 
-##################
-## Observations ##
-##################
+#########
+## OBS ##
+#########
 
 OBS_TREE_NODE_N_FEATURES: int = 13
 OBS_TREE_N_NODES: int = 1 + 2
---
+++ src/train.py
@@ -0,0 +1,13 @@
+import configs as Configs
+
+###
+
+
+def train():
+    pass
+
+
+###
+
+if __name__ == '__main__':
+    train()
