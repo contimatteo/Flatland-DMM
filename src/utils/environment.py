038720e9ac5b1c40f69c1369589d3e05f8af225b
DavideSangiorgi <davide.sangiorgi@outlook.com>
2021-09-09 22:38:33 +0200

    little changes, but should get rid of this

--- src/utils/environment.py
+++ src/utils/environment.py
@@ -2,36 +2,48 @@ import numpy as np
 import time
 
 from typing import Dict, Any, Tuple, List
-from flatland.envs.rail_env import RailEnv, EnvAgent, Grid4TransitionsEnum, RailAgentStatus
-from flatland.envs.rail_generators import random_rail_generator
+from flatland.envs.rail_env import RailEnv, EnvAgent, Grid4TransitionsEnum
+from flatland.envs.rail_generators import random_rail_generator, sparse_rail_generator
 from flatland.utils.rendertools import AgentRenderVariant, RenderTool
+from flatland.envs.schedule_generators import sparse_schedule_generator
+from flatland.envs.malfunction_generators import MalfunctionParameters, ParamMalfunctionGen
 
 import configs as Configs
 
-from schemes.action import HighLevelAction
+from schemes.action import HighLevelAction, LowLevelAction
 from schemes.node import Node
 
 ###
 
 
 class RailEnvWrapper:
-    def __init__(self, observator):
+    def __init__(
+            self,
+            observator,
+            rail_generator,
+            schedule_generator,
+            malfunction_generator):
+
+
         self._info = None
         self._done = None
 
         self._observator = observator
+        self.rail_generator = rail_generator
+        self.schedule_generator = schedule_generator
+        self.malfunction_generator = malfunction_generator
 
         self._rail_env = RailEnv(
-            width=Configs.RAIL_ENV_WIDTH,
-            height=Configs.RAIL_ENV_HEIGHT,
-            rail_generator=random_rail_generator(),
-            # schedule_generator=None,
-            number_of_agents=Configs.N_OF_AGENTS,
+            width=Configs.MAP_WIDTH,
+            height=Configs.MAP_HEIGHT,
+            rail_generator=self.rail_generator,
+            schedule_generator=self.schedule_generator,
+            number_of_agents=Configs.N_AGENTS,
             obs_builder_object=self._observator,
             # malfunction_generator_and_process_data=None,
-            # malfunction_generator=None,
-            remove_agents_at_target=True,
-            random_seed=Configs.RANDOM_SEED,
+            malfunction_generator=self.malfunction_generator,
+            remove_agents_at_target=Configs.REMOVE_AGENTS_AT_TARGET,
+            random_seed=Configs.SEED,
             # record_steps=False,
             # close_following=True
         )
@@ -64,44 +76,31 @@ class RailEnvWrapper:
     def get_agent(self, agent_index: int) -> EnvAgent:
         return self._rail_env.agents[agent_index]
 
-    def get_agent_position(self, agent: EnvAgent) -> Tuple[int, int]:
+    def get_agent_position(self, agent_index: int) -> Tuple[int, int]:
         """
         maybe not so easy:
-        - if agent.status == READY_TO_DEPART the agent is already asking for observations and answering with
-          some decisions, but its position in still None
-          ==> in this case it's maybe better to return agent.initial_position
-        - we have 2 cases when the agent.position==None (agent.status==READY_TO_DEPART & agent.status==DONE_REMOVED),
-          maybe we want to distinguish those
+            - if agent.status == READY_TO_DEPART the agent is already asking for observations and answering with
+                some decisions, but its position in still None
+                ==> in this case it's maybe better to return agent.initial_position
+            - we have 2 cases when the agent.position==None (agent.status==READY_TO_DEPART & agent.status==DONE_REMOVED),
+                maybe we want to distinguish those
 
         remember also to not use agent.position during observations (agent.old_position becomes the correct one)
         """
-        if agent.status == RailAgentStatus.READY_TO_DEPART:
-            return agent.initial_position
-        elif agent.status == RailAgentStatus.DONE_REMOVED:
-            return None  # TODO: reason about this ...
-        else:
-            return agent.position
+        return self.get_agent(agent_index).position
 
-    def get_agent_direction(self, agent: EnvAgent) -> Grid4TransitionsEnum:
-        if agent.status == RailAgentStatus.READY_TO_DEPART:
-            return agent.initial_direction
-        elif agent.status == RailAgentStatus.DONE_REMOVED:
-            return None  # TODO: reason about this ...
-        else:
-            return agent.direction
+    def get_agent_direction(self, agent_index: int) -> Grid4TransitionsEnum:
+        return self.get_agent(agent_index).direction
 
-    def get_agent_allowed_directions(self, agent: EnvAgent) -> Tuple[bool]:
-        position = self.get_agent_position(agent)
-        direction = self.get_agent_direction(agent)
+    def get_agent_allowed_directions(self, agent_index: int) -> List[bool]:
+        position = self.get_agent_position(agent_index)
+        orientation = self.get_agent_direction(agent_index)
 
-        if position is None or direction is None:
+        if position is None:
             return [False, False, False, False]
 
-        ### this considers also the agent direction
-        ### (switches allow to turn only from specific directions)
-        directions = self._rail_env.rail.get_transitions(*position, direction)
-
-        return tuple([x == 1 for x in list(directions)])
+        # the following considers also the agent direction (switches allow to turn only from specific directions)
+        return self._rail_env.rail.get_transitions(*position, orientation)
 
     ###
 
@@ -113,37 +112,31 @@ class RailEnvWrapper:
 
         return observations
 
-    def step(
-        self, high_level_actions: Dict[int, HighLevelAction]
-    ) -> Tuple[Dict[int, Node], Dict[int, float]]:
+    def step(self, actions: Dict[int, HighLevelAction]) -> Tuple[Dict[int, Node], Dict[int, float]]:
         # TODO: convert high-level actions to low-level actions
         # ...
 
-        low_level_actions = high_level_actions.copy()
-
         ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
         # print()
-        # print()
         # print("==================================================================================")
-        agent: EnvAgent = self.get_agent(0)
-        direction = self.get_agent_direction(agent)
-        allowed_directions = self.get_agent_allowed_directions(agent)
-        high_level_action = low_level_actions[0]
-        # print("HIGH-LEVEL action = ", high_level_action, int(high_level_action))
-        low_level_action = high_level_action.to_low_level(direction, allowed_directions)
-        # print("LOW-LEVEL action = ", low_level_action, int(low_level_action))
-        low_level_actions[0] = low_level_action
+        # agent_index = 0
+        # print(self.get_agent_position(agent_index))
+        # print(self.get_agent_direction(agent_index))
+        # print(self.get_agent_allowed_directions(agent_index))
         # print("==================================================================================")
-        # print()
-        # print()
-        # input("Press Enter to continue...")
         # print()
         ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
 
-        observations, rewards, self._done, self._info = self._rail_env.step(low_level_actions)
+        # convert high level actions to low level
+        low_lev_actions = {i: HighLevelAction(actions.get(i)).to_low_level(self.get_agent_direction(i),
+                                                                           self.get_agent_allowed_directions(i))
+                          for i in actions}
+
+
+        observations, rewards, self._done, self._info = self._rail_env.step(low_lev_actions)
 
         if Configs.EMULATOR_ACTIVE is True:
             self._emulator.render_env(show=True, show_observations=True, show_predictions=False)
             time.sleep(Configs.EMULATOR_STEP_TIMEBREAK_SECONDS)
 
-        return observations, rewards
+        return observations, rewards, self._done, self._info
